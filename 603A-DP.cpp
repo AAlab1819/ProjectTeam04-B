3c49053dc0e35a19c198d4be201066a84c50cf92
santosonicholas <42811915+santosonicholas@users.noreply.github.com>
2018-11-24 00:05:26 +0700

    Update 603A-DP.cpp

--- 603A-DP.cpp
+++ 603A-DP.cpp
@@ -1,25 +1,33 @@
 #include <bits/stdc++.h>
+#include <omp.h>>
 using namespace std;
 
 
 int main() {
- 
-    int n; 
+	double startTime, endTime;
+    int n;
 	cin >> n;
-	
-	int dp[n][3];
-	
+
+	vector<  vector<int> > dp(n,vector<int>(3));
+
+//	int dp[n][3];
+
     string s;
     cin >> s;
+	//cout<<n<<" "<<s.length()<<endl;
+	//startTime = omp_get_wtime();
 
     dp[0][0] = dp[0][1] = dp[0][2] = 1;
-    
+
     for(int i=1;i<s.length();i++){
         dp[i][0] = dp[i-1][0] + (s[i]!=s[i-1]);
         dp[i][1] = max (dp[i-1][0] + (s[i]==s[i-1]) , dp[i-1][1] + (s[i]!=s[i-1]));
         dp[i][2] = max(dp[i-1][1] + (s[i]==s[i-1]) , dp[i-1][2] + (s[i]!=s[i-1]));
     }
-    
-    cout <<dp[n-1][2];
+
+	//endTime=omp_get_wtime();
+
+    cout <<dp[n-1][2]<<endl;
+    //cout<<fixed<<setprecision(100)<<endTime-startTime<<endl;
     return 0;
 }
