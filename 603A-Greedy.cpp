776fb6ed30818eb25dd8eb0072969b8c8e6e60e2
santosonicholas <42811915+santosonicholas@users.noreply.github.com>
2018-11-24 00:05:04 +0700

    Update 603A-Greedy.cpp

--- 603A-Greedy.cpp
+++ 603A-Greedy.cpp
@@ -1,16 +1,23 @@
 #include <bits/stdc++.h>
+#include <omp.h>
 using namespace std;
 
 int main(){
-	
+	double startTime, endTime;
+
 	int N, res = 1;
 	string S;
 
   	cin >> N >> S;
-  
+	
+  	//cout<<N<<" "<<S.length()<<endl;
+  	//startTime = omp_get_wtime();
+	
   	for(int i = 1; i < N; i++){
   	  res += (S[i] != S[i - 1]);
  	 }
-  
- 	 cout << min(res + 2, N) << '\n';
+	
+    //endTime=omp_get_wtime();
+ 	 cout << min(res + 2, N) /*<< '\n'*/;
+ 	 //cout<<fixed<<setprecision(100)<<endTime-startTime<<endl;
 }
